4460c4acb7a7c1daccad4f79247d11a45564b16f
Sriram Koritala <sriramk3@illinois.edu>
2025-04-26 22:02:06 -0500

    suspense bounderies

--- src/app/account/create/page.tsx
+++ src/app/account/create/page.tsx
@@ -1,9 +1,12 @@
 import AccountCard from "@/components/ui/auth/AccountCard";
+import { Suspense } from 'react';
 
 export default function Create() {
   return (
     <div>
-      <AccountCard />
+      <Suspense>
+        <AccountCard />
+      </Suspense>
     </div>
   );
 }
--- src/app/account/login/page.tsx
+++ src/app/account/login/page.tsx
@@ -1,9 +1,12 @@
 import SignInCard from "@/components/ui/auth/SignInCard";
+import { Suspense } from 'react';
 
 export default function Login() {
   return (
     <div>
-      <SignInCard />
+      <Suspense>
+        <SignInCard />
+      </Suspense>
     </div>
   );
 }
